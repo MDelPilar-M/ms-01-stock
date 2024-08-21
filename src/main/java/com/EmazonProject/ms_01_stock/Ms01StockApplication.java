7418771f166ec9cac626cefe658a2582f39800d1
MDelPilar-M <mp548730@gmail.com>
2024-08-20 23:19:12 -0500

    starting proyect

---
+++ src/main/java/com/EmazonProject/ms_01_stock/Ms01StockApplication.java
@@ -0,0 +1,13 @@
+package com.EmazonProject.ms_01_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms01StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms01StockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmazonProject/ms_01_stock/Ms01StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmazonProject.ms_01_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms01StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
